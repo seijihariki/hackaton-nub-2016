53a3a3b0531d2930afc72294496bbece26cb9e05
BrunoBBS <bruno.bbs3@hotmail.com>
2016-08-28 04:45:08 -0300

    fk

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
 	$user   = $_POST['username'];
 	$pass 	= $_POST["password"];
 
-	$result = mysqli_query($connection, "SELECT salt FROM users WHERE user=".$user);
+	$result = mysqli_query($connection, "SELECT salt FROM users WHERE user=\"".$user."\"");
 	$salt = "";
 	if (mysqli_num_rows($result) == 1)
 	{
@@ -27,7 +27,7 @@
 
 	$pass_hash = sha1($salt.$pass);
 
-	$result = mysqli_query($connection, "SELECT * FROM users WHERE user=".$user." AND pass=".$pass_hash);
+	$result = mysqli_query($connection, "SELECT * FROM users WHERE user=\"".$user."\"" AND pass=\"".$pass_hash."\"");
 
 	if (mysqli_num_rows($result) == 1)
 	{
