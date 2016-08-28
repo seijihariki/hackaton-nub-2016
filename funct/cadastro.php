621a24d37a120cf9eff7c5c63c518dab58876da3
Victor Seiji Hariki <victorseijih@gmail.com>
2016-08-28 06:05:52 -0300

    WOW

---
+++ funct/cadastro.php
@@ -0,0 +1,49 @@
+<?php
+	error_reporting(E_ERROR | E_PARSE);
+	$server = "mysql.hostinger.com.br";
+	$dbuser = "u658685063_adm";
+	$dbpass = "senhadb";
+	$dbname = "u658685063_users";
+
+	function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+	}	
+
+	$connection = mysqli_connect($server, $dbuser, $dbpass, $database);
+	
+	if (!$connection) {
+		die("{\"status\": \"error\"}");
+	}
+
+	$user   = $_POST['username'];
+	$pass 	= $_POST["password"];
+
+	$result = mysqli_query($connection, "SELECT id FROM users WHERE user=\"".$user."\"");
+	
+
+	if (mysqli_num_rows($result) == 1)
+	{
+		mysql_close($connection);
+		exit("{\"status\": \"already\"}");
+	} else {
+		$salt = generateRandomString();
+		$pass_hash = sha1($salt.$pass);
+		$result = mysqli_query($connection, "INSERT INTO users (user,password,salt) VALUES (\"".$user."\",\"".$pass_hash."\",\"".$salt."\")");
+		if ($result)
+		{
+		mysql_close($connection);
+		exit("{\"status\": \"OK\"}");
+		} else {
+			mysql_close($connection);
+				die("{\"status\": \"error\"}");
+		}	
+	}
+
+
+?>
