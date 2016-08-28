70e228110c4302c9c827eff74e935848c135220f
BrunoBBS <bruno.bbs3@hotmail.com>
2016-08-28 04:50:28 -0300

    d

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
 
 	$result = mysqli_query($connection, "SELECT salt FROM users WHERE user=\"".$user."\"");
 	$salt = "";
-	if (mysqli_num_rows($result) == 1)
+	if (mysqli_num_rows($result) > 0)
 	{
 		$row = $result->fetch_assoc();
 		$salt = $row["salt"];
@@ -29,7 +29,7 @@
 
 	$result = mysqli_query($connection, "SELECT * FROM users WHERE user=\"".$user."\" AND pass=\"".$pass_hash."\"");
 
-	if (mysqli_num_rows($result) == 1)
+	if (mysqli_num_rows($result) > 0)
 	{
 		session_start();
 		$_SESSION["user"] = $user;
